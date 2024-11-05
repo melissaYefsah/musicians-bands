cbc091f8c838a89e7134c7106eea4ee31424963f
Melissa Yefsah <melissa.yefsah@melissa.yefsah-MacBook-Pro>
2024-11-05 13:21:46 -0500

    Added the Models and their tests

--- db.js
+++ db.js
@@ -1,9 +1,17 @@
-const path = require('path');
-const { Sequelize, Model } = require('sequelize');
+//TODO - create the new sequelize connection
+    const path = require("path");
+    const { Sequelize, Model, DataTypes } = require("sequelize");
 
-// TODO - create the new sequelize connection
+    const sequelize = new Sequelize({
+        dialect:"sqlite",
+        storage:path.join(__dirname,"sequelize.sqlite")
+    })
+
+
 
 module.exports = {
     sequelize,
-    Sequelize
+    Sequelize,
+    Model,
+    DataTypes
 };
