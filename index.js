f17875516614ee22d83b564edc1c8b425a44341c
Melissa Yefsah <melissa.yefsah@melissa.yefsah-MacBook-Pro>
2024-11-07 12:54:13 -0500

    Added Associations and Tests

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const { Musician } = require('./models/Musician')
 const { Song } = require("./models/Song")
 // Define associations here
 
+//Multiple musicians can be added to a band
+//Every musician has only one band
+Band.hasMany(Musician);
+Musician.belongsTo(Band);
+
+//Multiple songs can be added to a Band.
+//Multiple bands can have the same Song.
+
+Band.hasMany(Song);
+Song.hasMany(Band);
+
+
+
+
 
 
 module.exports = {
